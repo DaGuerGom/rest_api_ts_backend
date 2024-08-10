3d66aabaefbff5f71c4bd503bf4bb7f5293ce88c
David Guerrero Gómez <95078399+DaGuerGom@users.noreply.github.com>
2024-08-10 21:18:30 +0200

    First Commit

---
+++ src/handlers/product.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from "express"
+import Product from "../models/Product.model"
+
+export const getProducts=async(req:Request,res:Response)=>{
+    try {
+        const products=await Product.findAll({
+            order:[
+                ['price','DESC']
+            ]
+        })
+        res.json({data:products})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getProductById=async(req:Request,res:Response)=>{
+    try {
+        const product=await Product.findByPk(req.params.id)
+        if (!product) {
+            return res.status(404).json({
+                error:'Producto No Encontrado'
+            })
+        }
+        res.json({data:product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const createProduct=async(req:Request,res:Response)=>{
+    try {
+        const product=await Product.create(req.body)
+        res.status(201).json({data:product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateProduct=async(req:Request,res:Response)=>{
+    try {
+        const product=await Product.findByPk(req.params.id)
+        if (!product) {
+            return res.status(404).json({
+                error:'Producto No Encontrado'
+            })
+        }
+        await product.update(req.body)
+        await product.save()
+        res.json({data:product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateAvailability=async(req:Request,res:Response)=>{
+    try {
+        const product=await Product.findByPk(req.params.id)
+        if (!product) {
+            return res.status(404).json({
+                error:'Producto No Encontrado'
+            })
+        }
+        product.availability=!product.availability
+        await product.save()
+        res.json({data:product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deleteProduct=async(req:Request,res:Response)=>{
+    try {
+        const product=await Product.findByPk(req.params.id)
+        if (!product) {
+            return res.status(404).json({
+                error:'Producto No Encontrado'
+            })
+        }
+        await product.destroy()
+        res.json({data:"Producto Eliminado"})
+    } catch (error) {
+        console.log(error)
+    }
+}
